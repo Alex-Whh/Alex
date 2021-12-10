68ae2009898790e003edc6c0e280714281536e14
Alex-Whh <95912642+Alex-Whh@users.noreply.github.com>
2021-12-10 18:45:29 +0800

    Add files via upload

---
+++ area/doc/area.R
@@ -0,0 +1,12 @@
+## ---- include = FALSE---------------------------------------------------------
+knitr::opts_chunk$set(
+  collapse = TRUE,
+  comment = "#>"
+)
+
+## ----setup--------------------------------------------------------------------
+## Import library (area)
+library(area)
+##Input the radius of the circle which you wants to know the area of it, the function has default 1
+area_circle()
+
